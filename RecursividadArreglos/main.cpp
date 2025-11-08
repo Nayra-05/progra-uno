79da1549312c2dcd3773f1b7e1f6890f001255b7
Nayra-05 <nayragrios@gmail.com>
2025-11-08 10:34:02 -0400

    Add files via upload

---
+++ RecursividadArreglos/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+//iterativa
+void sumaArregloIterativo(int n, int vec[]){
+        int sum=0;
+    for(int i=0;i<n;i++){
+        sum= sum+vec[i];
+    }
+    cout << "La suma es: " << sum << endl;
+}
+
+int arreglosRecurivo(int vec[], int n){
+    int sum;
+    if(n==0){
+        return vec[0];
+    }else{
+    sum =vec[n]+arreglosRecurivo(vec,n-1);
+   }
+   return sum;
+}
+int main()
+{
+    int vec[5]={1,2,3,4,5};
+   cout<< arreglosRecurivo(vec,4) <<endl;
+    //sumaArregloIterativo(5,vec);
+    return 0;
+}
